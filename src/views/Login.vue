f3d8e5be9b15b6c1fd208b63efeda43cca87bb57
Juan Gomez <go.juangomez23@gmail.com>
2020-05-30 13:01:22 -0500

    configuracion inicial

---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <div class="">
+      <pre>user</pre>
+      <form>
+        <div class="row">
+            <input type="text" name="" v-model="form.email">
+        </div>
+        <br>
+        <div class="row">
+            <input type="password" name="" v-model="form.password">
+        </div>
+        <br>
+        <div class="row">
+          <button type="button" name="button" @click="login">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    data(){
+      return {
+        user: null,
+          form: {
+            email:'juan@gmail.com',
+            password: 123456
+          }
+      }
+    },
+    methods: {
+      login(){
+        console.log('eveinado',this.form);
+        
+      }
+    }
+  }
+</script>
